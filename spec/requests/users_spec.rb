3e337f0915789d125e868c2436a51006d3e2ed64
izzyyadams <130385979+izzyyadams@users.noreply.github.com>
2025-11-05 21:17:06 -0500

    DELETE /login request spec refactored

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,36 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe "User logout", type: :request do
+
+    #function to stup login and logout since sessions are not implemented yet
+    #this should be removed once login is functioning
+    def stub_user(user)
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    end
+
     describe "DELETE /logout" do
         context "the user is logged in" do
             before do
-                allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(double("User", id: 3)) #stubbing login
+                stub_user(double("User", id: 3)) #stubbing login
             end
 
-            it "prevents access to protected pages" do
-                allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(double("User", id: 3)) #stubbing login
+            it "prevents access to protected pages after logout" do
                 delete logout_path
-                allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(nil) #stubbing logout since sessions don't exist yet, will happen in destroy path, code is there
-                get itineraries_path
-                expect(response).to redirect_to(login_path)
+                stub_user(nil) #stubbing logout since sessions don't exist yet, will happen in destroy path, code is there
+                get itineraries_path # visit a protected page (should be denied access)
+                expect(response).to redirect_to(login_path) # when access is denied it redirects to login page
             end
 
             it "redirects to the login page" do
-                delete logout_path
-                expect(response).to redirect_to(login_path)
+                delete logout_path 
+                expect(response).to redirect_to(login_path) #logging out redirects to the login page
             end
         end
 
-        context "the user is logged out" do
+        context "the user is already logged out" do
             before do
-                allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(nil)
+               stub_user(nil)
             end
             it "redirects to the login page" do
                 delete logout_path
                 expect(response).to redirect_to(login_path)
             end
             it "prevents access to protected pages" do
-                delete logout_path
                 get itineraries_path
                 expect(response).to redirect_to(login_path)
             end
