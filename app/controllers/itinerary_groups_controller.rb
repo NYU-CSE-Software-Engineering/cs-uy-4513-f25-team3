77f093d98794f235cc7150989ce0b0134495b875
GabrielD264 <gpd264@nyu.edu>
2025-11-06 23:04:52 -0500

    Make PATCH /itinerary_groups/:id request spec pass

--- app/controllers/itinerary_groups_controller.rb
+++ app/controllers/itinerary_groups_controller.rb
@@ -2,4 +2,23 @@ class ItineraryGroupsController < ApplicationController
   def edit
     @itinerary_group = ItineraryGroup.find(params[:id])
   end
+  def update
+    @itinerary_group = ItineraryGroup.find(params[:id])
+    
+    if @itinerary_group.update(itinerary_group_params)
+      redirect_to itinerary_group_path(@itinerary_group)
+    else
+      render :edit
+    end
+  end
+  
+  def show
+    @itinerary_group = ItineraryGroup.find(params[:id])
+  end
+  
+  private
+  
+  def itinerary_group_params
+    params.require(:itinerary_group).permit(:title)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  resources :itinerary_groups, only: [:edit]
+  resources :itinerary_groups, only: [:edit, :update, :show]
 end
