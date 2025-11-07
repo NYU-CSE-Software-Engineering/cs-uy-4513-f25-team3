fba2460f58248e1ca6f31c85f32ec38a7b06ec78
Jason Lin <8333jlin@gmail.com>
2025-11-07 14:28:50 -0500

    add missing step definitions

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -33,3 +33,20 @@ Then('I should not see a {string} button') do |button_text|
   expect(page).not_to have_link(button_text)
 end
 
+When('I edit MessageID {int} to {string}') do |id, new_text|
+  message = Message.find(id)
+  message.update!(content: new_text)
+end
+
+When('I press {string} on MessageID {int}') do |button_text, id|
+  message = Message.find(id)
+  click_button(button_text, match: :first)
+end
+
+Given('I am on the {string} page') do |page_name|
+  visit path_for(page_name)
+end
+
+Given('I am on the itineraries page') do
+  visit itineraries_path
+end
