78cebc6f8a8709073607ad4ebdc7ef1d88439c14
GabrielD264 <gpd264@nyu.edu>
2025-11-06 22:09:12 -0500

    Add failing request spec for PATCH /itinerary_groups/:id

--- spec/requests/itinerary_groups_spec.rb
+++ spec/requests/itinerary_groups_spec.rb
@@ -1,14 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe "ItineraryGroups", type: :request do
-  describe "GET /itinerary_groups/:id/edit" do
-    it "renders the edit template (settings page)" do
-      itinerary_group = ItineraryGroup.create!(title: "NYC Tour")
-      
-      get edit_itinerary_group_path(itinerary_group)
-      
-      expect(response).to have_http_status(:success)
-      expect(response).to render_template(:edit)
+    describe "GET /itinerary_groups/:id/edit" do
+        it "renders the edit template (settings page)" do
+            itinerary_group = ItineraryGroup.create!(title: "NYC Tour")
+            
+            get edit_itinerary_group_path(itinerary_group)
+            
+            expect(response).to have_http_status(:success)
+            expect(response).to render_template(:edit)
+        end
     end
-  end
+
+
+    describe "PATCH /itinerary_groups/:id" do
+        it "redirects to the show page after successful update" do
+            itinerary_group = ItineraryGroup.create!(title: "NYC Tour")
+            
+            patch itinerary_group_path(itinerary_group), params: {
+                itinerary_group: { title: "NYC Tour Updated" }
+            }
+            
+            expect(response).to redirect_to(itinerary_group_path(itinerary_group))
+        end
+    end
 end
