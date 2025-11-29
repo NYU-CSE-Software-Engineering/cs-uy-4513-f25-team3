0c84a97bd1fac1e41cc63e6d90f4cb1500cb241a
Jason Lin <8333jlin@gmail.com>
2025-11-29 14:15:32 -0500

    remove duplicate steps

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -11,15 +11,6 @@ Then(/^I should see (a message|an error) "(.*)"$/) do |type, text|
   expect(page).to have_content(text)
 end
 
-
-When('I click {string}') do |submit|
-  click_button submit
-end
-
-Then(/^I should see (?:a message|an error) "(.*)"$/) do |type, text|
-  expect(page).to have_content(text)
-end
-
 Then(/^I should be on the (.*) page$/) do |page_name|
   expect(page).to have_current_path(path_to(page_name))
 end
