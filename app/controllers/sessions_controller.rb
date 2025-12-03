e50ec8d6d9a2be33bf64283e2d57dd1809235cbb
izzyyadams <130385979+izzyyadams@users.noreply.github.com>
2025-12-02 22:47:32 -0500

    added security measure

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
     def create
         user = User.find_by(username: params[:user][:username])
         if user && user.password == params[:user][:password]
+            reset_session
             session[:user_id] = user.id
             redirect_to itineraries_path
         else
