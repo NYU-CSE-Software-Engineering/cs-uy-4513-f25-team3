3eff03332ed01e037c443b235d2a5f3b4845fbe3
cjin0711 <cj0711888@gmail.com>
2025-12-06 18:09:47 -0500

    added failing model spec for itinerary end date validation

--- spec/models/itinerary_group_spec.rb
+++ spec/models/itinerary_group_spec.rb
@@ -18,4 +18,10 @@ RSpec.describe ItineraryGroup, type: :model do
     expect(itinerary_group).not_to be_valid
     expect(itinerary_group.errors[:start_date]).to include("can't be blank")
   end
+
+  it 'is invalid without a end_date' do
+    itinerary_group = ItineraryGroup.new
+    expect(itinerary_group).not_to be_valid
+    expect(itinerary_group.errors[:end_date]).to include("can't be blank")
+  end
 end
