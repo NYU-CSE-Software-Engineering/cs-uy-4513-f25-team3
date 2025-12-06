454a644e3818d4fa6429ddb4a19f7a1da7b659ab
Muathmua <moath.animations@Gmail.com>
2025-12-05 22:41:28 -0500

    Updated user_spec.rb to account for "role" attribute

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,9 +2,13 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
+    it 'is valid with username, password, and role' do
+        user = User.new(username: "freddy", password: "pass123", password_confirmation: "pass123", role: "user")
+        expect(user).to be_valid
+    end
 
     it 'is invalid without a Username' do
-        user = User.new(password: "abc123")
+        user = User.new(password: "abc123", role: "user")
         expect(user).not_to be_valid
     end
     it 'is invalid if Username already exists' do
@@ -13,12 +17,13 @@ RSpec.describe User, type: :model do
         expect(duplicate).not_to be_valid
     end
     it 'is invalid without a Password' do
-        user = User.new(username: "john123")
+        user = User.new(username: "john123", role: "user")
         expect(user).not_to be_valid
     end
     it 'is invalid without a role' do
-        user = User.new(username: 'testuser', password: 'password123')
+        user = User.new(username: 'testuser', password: 'password123', password_confirmation: "password123")
         expect(user).not_to be_valid
     end
 
 end
+
