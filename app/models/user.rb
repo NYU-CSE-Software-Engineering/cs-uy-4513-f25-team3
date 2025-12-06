0409c05540128f4c481cb70d464f1f056f33a577
Muathmua <moath.animations@Gmail.com>
2025-12-06 01:15:12 -0500

    cucumber green, updates to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,17 @@
 class User < ApplicationRecord
+  attr_accessor :password, :password_confirmation
+
   validates :role, presence: true
   validates :username, presence: true, uniqueness: true
-  validates :password, presence: true
-end
+  validates :password, presence: true, on: :create
+  validates :password_confirmation, presence: true, on: :create
+  validate  :passwords_match, on: :create
 
+  private
+
+  def passwords_match
+    if password != password_confirmation
+      errors.add(:password_confirmation, "doesn't match Password")
+    end
+  end
+end
