8e47fca13a101953f258e9bbd0d1f57b3c058636
cjin0711 <cj0711888@gmail.com>
2025-12-06 18:42:43 -0500

    made itinerary chronological dates validation spec pass

--- app/models/itinerary_group.rb
+++ app/models/itinerary_group.rb
@@ -3,6 +3,14 @@ class ItineraryGroup < ApplicationRecord
   validates :location, presence: true
   validates :start_date, presence: true
   validates :end_date, presence: true
+  validate :chronological_dates
+
+  def chronological_dates
+    return if start_date.blank? || end_date.blank?
+    if end_date < start_date
+      errors.add(:end_date, "must be same as or after start date")
+    end
+  end
 
   def trip_type
     is_private? ? "Private" : "Public"
