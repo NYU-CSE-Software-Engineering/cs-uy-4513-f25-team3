9b52a491812ab2593e2d986b16be011de588813e
cjin0711 <cj0711888@gmail.com>
2025-12-06 14:02:59 -0500

    added failing model spec for itinerary location validation

--- spec/models/itinerary_group_spec.rb
+++ spec/models/itinerary_group_spec.rb
@@ -1,9 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe ItineraryGroup, type: :model do
-  it 'is invalid without a group_name' do
+  it 'is invalid without a title' do
     itinerary_group = ItineraryGroup.new
     expect(itinerary_group).not_to be_valid
     expect(itinerary_group.errors[:title]).to include("can't be blank")
   end
+
+  it 'is invalid without a location' do
+    itinerary_group = ItineraryGroup.new
+    expect(intinerary_group).not_to be_valid
+    expect(itinerary_group.errors[:location]).to include("can't be blank")
+  end
+  
 end
