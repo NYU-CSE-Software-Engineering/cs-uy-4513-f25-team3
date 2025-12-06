71155f382ffa3d1a6febe099cbc62055f3209087
Jason Lin <8333jlin@gmail.com>
2025-12-05 20:53:26 -0500

    add date fields to make spec pass again

--- spec/requests/itinerary_groups_spec.rb
+++ spec/requests/itinerary_groups_spec.rb
@@ -3,7 +3,11 @@ require 'rails_helper'
 RSpec.describe "ItineraryGroups", type: :request do
     describe "GET /itineraries/:id/edit" do
         it "renders the edit template (settings page)" do
-            itinerary_group = ItineraryGroup.create!(title: "NYC Tour")
+            itinerary_group = ItineraryGroup.create!(
+                title: "NYC Tour",
+                start_date: Date.today,
+                end_date: Date.today + 1
+            )
             
             get edit_itinerary_path(itinerary_group)
             
@@ -15,7 +19,11 @@ RSpec.describe "ItineraryGroups", type: :request do
 
     describe "PATCH /itineraries/:id" do
         it "redirects to the show page after successful update" do
-            itinerary_group = ItineraryGroup.create!(title: "NYC Tour")
+            itinerary_group = ItineraryGroup.create!(
+                title: "NYC Tour",
+                start_date: Date.today,
+                end_date: Date.today + 1
+            )
             
             patch itinerary_path(itinerary_group), params: {
                 itinerary_group: { title: "NYC Tour Updated" }
@@ -25,7 +33,11 @@ RSpec.describe "ItineraryGroups", type: :request do
         end
 
         it "sets a success flash message after successful update" do
-            itinerary_group = ItineraryGroup.create!(title: "NYC Tour")
+            itinerary_group = ItineraryGroup.create!(
+                title: "NYC Tour",
+                start_date: Date.today,
+                end_date: Date.today + 1
+            )
             
             patch itinerary_path(itinerary_group), params: {
                 itinerary_group: { title: "NYC Tour Updated" }
