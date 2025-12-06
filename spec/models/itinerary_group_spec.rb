2a5e1f5ef51e9373e4381aea1af3624b8a9a03c2
cjin0711 <cj0711888@gmail.com>
2025-12-06 18:37:43 -0500

    added failing model spec for itinerary chronological dates validation

--- spec/models/itinerary_group_spec.rb
+++ spec/models/itinerary_group_spec.rb
@@ -24,4 +24,15 @@ RSpec.describe ItineraryGroup, type: :model do
     expect(itinerary_group).not_to be_valid
     expect(itinerary_group.errors[:end_date]).to include("can't be blank")
   end
+
+  it 'is invalid if end_date is before start_date' do
+    itinerary_group = ItineraryGroup.new(
+      title: "Japan Adventure",
+      location: "Japan",
+      start_date: '2026-01-12',
+      end_date: '2026-01-11'
+    )
+    expect(itinerary_group).not_to be_valid
+    expect(itinerary_group.errors[:end_date]).to include("must be same as or after start date")
+  end
 end
