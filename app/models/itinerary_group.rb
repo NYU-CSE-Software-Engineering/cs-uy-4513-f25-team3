60d5438f0e901a8991f103307e22e0bfb3dba6cb
cjin0711 <cj0711888@gmail.com>
2025-12-06 19:41:17 -0500

    made itinerary past date validation spec pass

--- app/models/itinerary_group.rb
+++ app/models/itinerary_group.rb
@@ -4,11 +4,22 @@ class ItineraryGroup < ApplicationRecord
   validates :start_date, presence: true
   validates :end_date, presence: true
   validate :chronological_dates
+  validate :dates_not_in_past
 
   def chronological_dates
     return if start_date.blank? || end_date.blank?
     if end_date < start_date
       errors.add(:end_date, "must be same as or after start date")
+    end
+  end
+
+  def dates_not_in_past
+    current = Date.current
+    if start_date.present? && start_date < current
+      errors.add(:start_date, 'start date must be today or in the future')
+    end
+    if end_date.present? && end_date < current
+      errors.add(:end_date, 'end date must be today or in the future')
     end
   end
 
