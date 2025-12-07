ada4d4bc31e1581018e5d805e18b132d82f6d1a7
GabrielD264 <gpd264@nyu.edu>
2025-12-07 15:42:30 -0500

    Green: Added ItineraryGroup associations for attendees and organizer:

--- app/models/itinerary_group.rb
+++ app/models/itinerary_group.rb
@@ -1,6 +1,10 @@
 class ItineraryGroup < ApplicationRecord
   validates :title, presence: true
 
+  has_many :itinerary_attendees, dependent: :destroy
+  has_many :attendees, through: :itinerary_attendees, source: :user
+  belongs_to :organizer, class_name: 'User', foreign_key: 'organizer_id', optional: true
+
   def trip_type
     is_private? ? "Private" : "Public"
   end
