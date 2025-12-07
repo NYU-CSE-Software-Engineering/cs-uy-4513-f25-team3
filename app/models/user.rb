9f2f8cc45dee1f25e7de774b53bf869b1754dd7f
GabrielD264 <gpd264@nyu.edu>
2025-12-07 15:37:19 -0500

    Green: added user associations for itinerary groups

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ApplicationRecord
   validates :role, presence: true
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true
-end
 
+  has_many :itinerary_attendees, dependent: :destroy
+  has_many :itinerary_groups, through: :itinerary_attendees
+  has_many :organized_groups, class_name: 'ItineraryGroup', foreign_key: 'organizer_id', dependent: :destroy
+end
