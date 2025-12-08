2e5478f69c321c64e9a770bf46a6a759362598b0
Jason Lin <8333jlin@gmail.com>
2025-12-07 23:30:37 -0500

    add factories

--- spec/factories/hotels.rb
+++ spec/factories/hotels.rb
@@ -1,11 +1,10 @@
 FactoryBot.define do
   factory :hotel do
     sequence(:name) { |n| "Hotel #{n}" }
-    location { "New York" }
-    rating { 4 }
-    arrival_time { Time.zone.parse("2025-12-04 09:00:00") }
-    departure_time { Time.zone.parse("2025-12-12 13:00:00") }
-    cost { 300 }
+    location { Faker::Address.city }
+    rating { rand(1..5) }
+    arrival_time { Faker::Time.forward(days: 30, period: :afternoon) }
+    departure_time { arrival_time + rand(1..10).days }
+    cost { rand(80..500) }
   end
 end
-
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,11 @@
 FactoryBot.define do
-    factory :user do
-        username { "testuser" }
-        password { "password123" }
-        password_confirmation { "password123" } 
-        role { "user" }
-    end
+  factory :user do
+    first_name { Faker::Name.first_name }
+    last_name  { Faker::Name.last_name }
+    sequence(:username) { |n| "user#{n}" }
+    password { "password123" }
+    role { "user" }
+    age { rand(18..70) }
+    gender { %w[male female non_binary other].sample }
+  end
 end
